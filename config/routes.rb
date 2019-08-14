bb5e1426d35810aa9366f7ec95fe89514089709b
birdbird656 <clarityman92@gmail.com>
2019-08-14 16:17:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users do  only: [:index, :edit, :update] 
+  resources :users do
     collection do
       get :search
     end
